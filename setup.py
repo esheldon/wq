24564a4b6928af4856ffa585692664a6542917c6
Erin Sheldon <erin.sheldon@gmail.com>
2011-11-16 16:26:18 -0500

    update setup.py for new wq subdir

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 
 description = "A simple work queue that actually works."
 
-setup(name="awq", 
+setup(name="wq", 
       version="0.1.0",
       description=description,
       url="https://github.com/esheldon/awq",
       author="Erin Scott Sheldon, Anze Slosar",
       author_email="erin.sheldon@gmail.com",
       scripts=["wq/wq"],
-      packages=['awq'])
+      packages=['wq'])
 
 
 
