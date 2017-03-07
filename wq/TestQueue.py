ba6f93645d5fd426334237b88b81fd2956997023
Erin Sheldon <erin.sheldon@gmail.com>
2017-03-07 12:27:21 -0500

    python3 ifying

--- wq/TestQueue.py
+++ wq/TestQueue.py
@@ -1,39 +1,32 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 import server
 
 cluster = server.Cluster('bnl_desc.txt')
 
-# print a.Status()
-# a.Reserve(['astro0001','astro0001','astro0012'])
-# print a.Status()
-# a.Reserve(['astro0001','astro0001','astro0012'])
-# print a.Status()
-# a.Unreserve(['astro0001','astro0001','astro0012'])
-# a.Unreserve(['astro0001','astro0001','astro0012'])
-# print a.Status()
 
 req={}
 req['submit_mode']='bycore'
 req['N']=6
 
-print '-----------'
+print('-----------')
 
 j = server.Job ({'require':req,'pid':0})
-print cluster.Status()
+print(cluster.Status())
 j.match(cluster)
-print j['status']
-print cluster.Status()
+print(j['status'])
+print(cluster.Status())
 
-print '-----------'
+print('-----------')
 
 j2 = server.Job ({'require':req,'pid':1})
 j2.match(cluster)
-print j2['status']
-print cluster.Status()
+print(j2['status'])
+print(cluster.Status())
 
 
-print '-----------'
+print('-----------')
 
 req={}
 req['submit_mode']='bynode'
@@ -41,11 +34,11 @@ req['in_group']=['gen4']
 req['N']=6
 j3 = server.Job ({'require':req,'pid':2})
 j3.match(cluster)
-print j3['status']
-print cluster.Status()
+print(j3['status'])
+print(cluster.Status())
 
 
-print '-----------'
+print('-----------')
 
 req={}
 req['submit_mode']='byhost'
@@ -53,20 +46,20 @@ req['host']='astro0001'
 req['N']=10
 j4 = server.Job ({'require':req,'pid':3})
 j4.match(cluster)
-print j4['status']
-print cluster.Status()
+print(j4['status'])
+print(cluster.Status())
 
-print '-----------'
+print('-----------')
 
 j5 = server.Job ({'require':req,'pid':4})
 j5.match(cluster)
 
-print j4['status'], j5['status']
+print(j4['status'], j5['status'])
 j4.unmatch(cluster)
 j5.match(cluster)
-print j4['status'], j5['status']
-print cluster.Status()
-print '--------------'
+print(j4['status'], j5['status'])
+print(cluster.Status())
+print('--------------')
 
 
 
@@ -78,15 +71,15 @@ req['min_cores']=0
 req['N']=6
 j6 = server.Job ({'require':req},a)
 j6.match()
-print j6['status']
-print j6.cluster.Status()
+print(j6['status'])
+print(j6.cluster.Status())
 
 
 
 for x in [j,j2,j3,j4,j5]:
     x.unmatch(cluster)
-print cluster.Status()
+print(cluster.Status())
 
 j6.match()
-print j6['status']
-print j6.cluster.Status()
+print(j6['status'])
+print(j6.cluster.Status())
