f774e7790b8b3b7e9e08998cad2514c0d70ecd57
Erin Sheldon <erin.sheldon@gmail.com>
2020-05-28 13:53:55 -0400

    use setuptools and bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
-import distutils
-from distutils.core import setup
+from setuptools import setup
 
-description = "A simple work queue that actually works."
+description = "A simple work queue"
 
-setup(name="wq", 
-      version="0.1.0",
-      description=description,
-      url="https://github.com/esheldon/awq",
-      author="Erin Scott Sheldon, Anze Slosar",
-      author_email="erin.sheldon@gmail.com",
-      scripts=["bin/wq"],
-      packages=['wq'])
-
-
-
-
+setup(
+    name="wq",
+    version="0.2.0",
+    description=description,
+    url="https://github.com/esheldon/wq",
+    author="Erin Scott Sheldon, Anze Slosar",
+    author_email="erin.sheldon@gmail.com",
+    scripts=["bin/wq"],
+    packages=['wq'],
+)
